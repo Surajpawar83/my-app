1307e050117d926d5ae76281c75e1e7e22221ccb
Surajpawar83 <pawarsk88@gmail.com>
2022-12-19 21:18:49 +0530

    basic-multi line JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+//multi line JSX
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dicta
+      repellat ad omnis cupiditate officia neque minus maiores assumenda
+      voluptatibus id, quos quo expedita consectetur eos nostrum distinctio?
+      Autem, consequatur.
+    </h1>
+  );
 }
 
 export default App;

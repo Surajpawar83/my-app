117bd798ddd249b8dfc622c4a0373c02ec5f55d8
Surajpawar83 <pawarsk88@gmail.com>
2022-12-19 23:21:10 +0530

    interpolation-3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 function App() {
+  // Data Members
   let project = "Counter Application";
   let btnValue = "Click mee";
+
+  // Member :: Type is Functions
+  let clickme = () => alert();
+  let increment = () => {};
+  let decrement = () => {};
 
   return (
     <div>
       <h1> {project} </h1>
-      <input type="button" value={btnValue} />
+      <input type="button" value={btnValue} onClick={clickme} />
+
+      <input type="button" value="increment" onClick={increment} />
     </div>
   );
 }

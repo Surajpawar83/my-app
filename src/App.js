5bc182007f451f95bbc0e859fa08f5c2cc6c1f10
Surajpawar83 <pawarsk88@gmail.com>
2022-12-20 00:05:09 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import { useState } from "react";
 
 function App() {
-  //let counter = 10;
-  let [counter, setCounter] = useState(10);
+  let str1 = "Home";
+  let str2 = "Explore";
+  let str3 = "Notification";
+
+  let [counter, setCounter] = useState(1);
+
+  let [city, setCity] = useState("mumbai");
+
+  let changeCity = () => {
+    city = "Hello " + city;
+
+    setCity(city);
+  };
 
   let increment = () => {
-    counter = counter + 1;
-
-    //setting the changed value and showed on webpage
+    counter++;
+    console.log(counter);
 
     setCounter(counter);
   };
 
   return (
     <div>
-      <h1>Counter Application</h1>
-      <h1> {counter} </h1>
+      <h1>{city}</h1>
+      <input type="button" value="change city" onClick={changeCity} />
+      <hr />
+      <h1>{str1}</h1>
+      <h1>{str2}</h1>
+      <h1>{str3}</h1>
+      <h1>{counter}</h1>
       <input type="button" value="Increment" onClick={increment} />
     </div>
   );

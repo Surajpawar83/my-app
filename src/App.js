0a243be128ebb9b602091667aa53cdbf8b0cde77
Surajpawar83 <pawarsk88@gmail.com>
2022-12-20 01:43:23 +0530

    map-function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 function App() {
   let title = "Map Demo";
-  let user = { id: 1, city: "mumbai" };
+  let list = ["mumbai", "delhi"];
 
   return (
     <div>
       <h1> {title} </h1>
-      <h1> {user.city} </h1>
+
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
     </div>
   );
 }
 export default App;
+
+// [mumbai, delhi] => [<h1>mumbai</h1>, <h1>delhi</h1>]

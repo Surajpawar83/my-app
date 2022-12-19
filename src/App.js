051350da7d2ea20f2d533ce0ed465fc35d97dc71
Surajpawar83 <pawarsk88@gmail.com>
2022-12-19 21:21:47 +0530

    basic-multi tag

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-//multi line JSX
+//multi Tag
 
 function App() {
   return (
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dicta
-      repellat ad omnis cupiditate officia neque minus maiores assumenda
-      voluptatibus id, quos quo expedita consectetur eos nostrum distinctio?
-      Autem, consequatur.
-    </h1>
+    <div>
+      <h1>Div and h1</h1>
+    </div>
   );
 }
 

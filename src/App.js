0eb79079a15c645402a74c6e8cf94c0bfa03c28a
Surajpawar83 <pawarsk88@gmail.com>
2022-12-20 01:36:49 +0530

    map-fn

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import { useState } from "react";
-
 function App() {
-  let str1 = "Home";
-  let str2 = "Explore";
-  let str3 = "Notification";
-
-  let [counter, setCounter] = useState(1);
-
-  let [city, setCity] = useState("mumbai");
-
-  let changeCity = () => {
-    city = "Hello " + city;
-
-    setCity(city);
-  };
-
-  let increment = () => {
-    counter++;
-    console.log(counter);
-
-    setCounter(counter);
-  };
+  let title = "Map Demo";
+  let user = { id: 1, city: "mumbai" };
 
   return (
     <div>
-      <h1>{city}</h1>
-      <input type="button" value="change city" onClick={changeCity} />
-      <hr />
-      <h1>{str1}</h1>
-      <h1>{str2}</h1>
-      <h1>{str3}</h1>
-      <h1>{counter}</h1>
-      <input type="button" value="Increment" onClick={increment} />
+      <h1> {title} </h1>
+      <h1> {user.city} </h1>
     </div>
   );
 }
-
 export default App;

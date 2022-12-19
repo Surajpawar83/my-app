4cbc812c099486dfdd6e11be89273d7eee8c1312
Surajpawar83 <pawarsk88@gmail.com>
2022-12-19 21:27:29 +0530

    basic-multiple-JSX

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@
 function App() {
   return (
     <div>
-      <h1>Div and h1</h1>
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+    </div>
+  );
+}
+
+function Person() {
+  return (
+    <div>
+      <img src="https://picsum.photos/400" alt="" />
+      <h1>Person name</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        repellendus adipisci dignissimos tenetur possimus eaque architecto
+        similique fugiat nisi alias error autem doloribus porro numquam harum,
+        laudantium recusandae ut. Sint!
+      </p>
     </div>
   );
 }

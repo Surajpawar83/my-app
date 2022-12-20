24fa7765094a6c7e9911111a5b5e1d5b33ec6aed
Surajpawar83 <pawarsk88@gmail.com>
2022-12-20 15:55:39 +0530

    styling-inline, internal, external

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let title = "Map Demo";
-  let [list, setList] = useState([]);
-
-  let addItem = () => {
-    // alternate :: No DOM PLZ!
-    let textRef = document.querySelector("#textId1");
-
-    // logical part
-    let newList = [textRef.value, ...list];
-
-    // dom part
-    setList(newList);
-  };
+  let mystyle = { color: "white", background: "purple" };
 
   return (
     <div>
-      <h1> {title} </h1>
-      <input type="text" name="" id="textId1" />
-      <input type="button" value="Add Item Into List" onClick={addItem} />
+      <h1 className="beautify">Hello again!</h1>
 
-      {list.map((item) => (
-        <h1>{item}</h1>
-      ))}
+      <h1 style={mystyle}>Hello world</h1>
+
+      <h1 style={{ color: "white", backgroundColor: "red" }}>Hello</h1>
     </div>
   );
 }
+
 export default App;
